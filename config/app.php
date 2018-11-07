4cf767f65b43565377f99928813e2442bc230620
shxwcj <1131920105@qq.com>
2018-11-07 13:34:57 +0800

    update database,app,lang

--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'zh_CN',
 
     /*
     |--------------------------------------------------------------------------
@@ -167,6 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Barryvdh \ Debugbar \ ServiceProvider :: class,
 
         /*
          * Application Service Providers...
@@ -225,7 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        ' Debugbar '=>Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
